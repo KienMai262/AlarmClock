fc3a16a6828303c78d8448223b5e38074ce9491d
Anh Linh <linhnguyenanh75@gmail.com>
2025-04-15 22:28:59 +0700

    initiate quiz activity

---
+++ app/src/main/java/com/example/alarmclock/ui/quizActivity/QuizActivity.java
@@ -0,0 +1,16 @@
+package com.example.alarmclock.ui.quizActivity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.alarmclock.R;
+
+public class QuizActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        // Thêm logic hiển thị quiz tại đây
+    }
+}
